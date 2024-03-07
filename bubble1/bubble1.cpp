11f5171e446c10f0dacb74a145071fe18c35230d
taznimt <taznimanwar06@gmail.com>
2024-03-07 17:00:40 +0700

    membuat prosedur output

--- bubble1/bubble1.cpp
+++ bubble1/bubble1.cpp
@@ -50,6 +50,19 @@ void bubbleSortArray() { //prosedur untuk mengurutkan array dengan metode bubble
     } while (pass <= n); //step 5  
 }
 
+void display() {
+    cout << endl;
+    cout << "=============" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=============" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; //output setiap element array pada garis baru
+
+    }
+    cout << "jumalah pass = " << n - 1 << endl; // menampilkan total angka dari elemen yang benar
+    cout << endl;
+}
+
 
 
 int main()
